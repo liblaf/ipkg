c22b8f7bf2e8d981e77718cc3363bd2854cad7a9
Qin Li <30631553+liblaf@users.noreply.github.com>
2022-12-21 21:37:21 +0800

    fix(code): new package

---
+++ ipkg/pkg/code/__init__.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+BASE_URL = "https://packages.microsoft.com"
+KEY_NAME = "microsoft.asc"
+KEY_URL = f"{BASE_URL}/keys/{KEY_NAME}"
+APT_DIR: Path = Path("/etc/apt")
+TRUSTED_KEY_PATH = APT_DIR / "trusted.gpg.d" / "microsoft.gpg"
+SOURCES_LIST_PATH: Path = APT_DIR / "sources.list.d/vscode.list"
+SOURCES_LIST = f"deb [arch=amd64,arm64,armhf] {BASE_URL}/repos/code stable main"
