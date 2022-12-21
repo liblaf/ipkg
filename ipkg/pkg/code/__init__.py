5a1cfa5f8fe5076079f158d6b31909a765e550f7
Qin Li <30631553+liblaf@users.noreply.github.com>
2022-12-22 01:15:06 +0800

    fix(pkg.code): add extensions

--- ipkg/pkg/code/__init__.py
+++ ipkg/pkg/code/__init__.py
@@ -8,3 +8,22 @@ APT_DIR: Path = Path("/etc/apt")
 TRUSTED_KEY_PATH = APT_DIR / "trusted.gpg.d" / "microsoft.gpg"
 SOURCES_LIST_PATH: Path = APT_DIR / "sources.list.d/vscode.list"
 SOURCES_LIST = f"deb {BASE_URL}/repos/code stable main"
+EXTENSIONS: list[str] = [
+    "aaron-bond.better-comments",
+    "donjayamanne.python-extension-pack",
+    "eamodio.gitlens",
+    "esbenp.prettier-vscode",
+    "foxundermoon.shell-format",
+    "James-Yu.latex-workshop",
+    "llvm-vs-code-extensions.vscode-clangd",
+    "ms-python.isort",
+    "ms-vscode.cpptools-extension-pack",
+    "oderwat.indent-rainbow",
+    "redhat.vscode-xml",
+    "redhat.vscode-yaml",
+    "streetsidesoftware.code-spell-checker",
+    "tamasfe.even-better-toml",
+    "WakaTime.vscode-wakatime",
+    "wayou.vscode-todo-highlight",
+    "wwm.better-align",
+]
--- ipkg/pkg/code/install.py
+++ ipkg/pkg/code/install.py
@@ -3,7 +3,15 @@ import click
 from ...utils.download import download
 from ...utils.run import run
 from .. import DOWNLOADS, SHELL
-from . import KEY_NAME, KEY_URL, NAME, SOURCES_LIST, SOURCES_LIST_PATH, TRUSTED_KEY_PATH
+from . import (
+    EXTENSIONS,
+    KEY_NAME,
+    KEY_URL,
+    NAME,
+    SOURCES_LIST,
+    SOURCES_LIST_PATH,
+    TRUSTED_KEY_PATH,
+)
 
 
 @click.command()
@@ -14,3 +22,5 @@ def main() -> None:
     run("sudo", str(SHELL), "-c", f'echo "{SOURCES_LIST}" > "{SOURCES_LIST_PATH}"')
     run("sudo", "apt", "update")
     run("sudo", "apt", "install", NAME)
+    for extension in EXTENSIONS:
+        run("code", "--install-extension", extension, "--force")
