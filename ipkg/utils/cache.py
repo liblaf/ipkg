48124d99bdbc4a5d757280ff948d0336019ea91d
Qin Li <30631553+liblaf@users.noreply.github.com>
2022-12-26 15:40:31 +0800

    fix(cmd.load): add new option --dry-run

--- ipkg/utils/cache.py
+++ ipkg/utils/cache.py
@@ -12,10 +12,11 @@ class ShellFunction:
     body: list[str]
 
 
-CACHE_DIR = Path(
+CACHE_DIR: Path = Path(
     os.environ.get("IPKG_CACHE_DIR", default=Path.home() / ".cache" / "ipkg")
 )
-INDENT = "  "
+DRY_RUN: bool = False
+INDENT: str = "  "
 
 
 def tee(*values: object, file: typing.TextIO):
@@ -24,39 +25,39 @@ def tee(*values: object, file: typing.TextIO):
 
 
 def open_cache(args: list[str] = sys.argv[1:], encoding="utf-8") -> typing.TextIO:
-    sha1 = hashlib.sha1(
-        string=bytes(" ".join(args) + "\n", encoding=encoding)
-    ).hexdigest()
-    if CACHE_DIR:
+    if DRY_RUN:
+        return sys.stdout
+    else:
+        sha1 = hashlib.sha1(
+            string=bytes(" ".join(args) + "\n", encoding=encoding)
+        ).hexdigest()
         os.makedirs(CACHE_DIR, exist_ok=True)
         return open(file=CACHE_DIR / sha1, mode="w")
-    else:
-        return open(file=os.devnull, mode="w")
 
 
 def export(cache: typing.TextIO, env: dict[str, str]) -> None:
     for name, value in env.items():
-        tee(f'export {name}="{value}"', file=cache)
+        print(f'export {name}="{value}"', file=cache)
 
 
 def unset(cache: typing.TextIO, names: list[str]) -> None:
     for name in names:
-        tee(f"unset {name}", file=cache)
+        print(f"unset {name}", file=cache)
 
 
 def function(cache: typing.TextIO, functions: list[ShellFunction]) -> None:
     for shell_function in functions:
-        tee(f"function {shell_function.name} {{", file=cache)
+        print(f"function {shell_function.name} {{", file=cache)
         for line in shell_function.body:
-            tee(INDENT + line, file=cache)
-        tee("}", file=cache)
+            print(INDENT + line, file=cache)
+        print("}", file=cache)
 
 
 def unset_function(cache: typing.TextIO, names: list[str]) -> None:
     for name in names:
-        tee(f"unset -f {name}", file=cache)
+        print(f"unset -f {name}", file=cache)
 
 
 def exec(cache: typing.TextIO, commands: list[list[str]]) -> None:
     for args in commands:
-        tee(" ".join(args), file=cache)
+        print(" ".join(args), file=cache)
